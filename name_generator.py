f945a357b004d42a77574e3b71480de907cdd590
Bailey Posante <posanteb@oregonstate.edu>
2021-02-28 01:09:09 -0500

    puts code into a function

--- name_generator.py
+++ name_generator.py
@@ -1,29 +1,33 @@
 import sys, random
 
 
-print("Welcome to the D&D Name Generator. \n")
-print("A name for your brand new, on-the-spot NPC:\n")
+def name_generator():
 
-firstNames = []
-with open("firstnames.txt", "r") as infile:
-    for line in infile:
-        line = line.strip(".0123456789 ")
-        firstNames.append(line)
+    print("Welcome to the D&D Name Generator. \n")
+    print("A name for your brand new, on-the-spot NPC:\n")
 
-lastNames = []
-with open("lastnames.txt", "r") as infile:
-    for line in infile:
-        line = line.strip(".0123456789 ")
-        lastNames.append(line)
+    firstNames = []
+    with open("firstnames.txt", "r") as infile:
+        for line in infile:
+            line = line.strip(".0123456789 ")
+            firstNames.append(line)
 
-while True:
+    lastNames = []
+    with open("lastnames.txt", "r") as infile:
+        for line in infile:
+            line = line.strip(".0123456789 ")
+            lastNames.append(line)
 
-    firstName = random.choice(firstNames)
-    lastName = random.choice(lastNames)
-    print(f"{firstName}{lastName}")
+    while True:
 
-    try_again = input("\nTry again? (Press Enter, else press n to quit)\n ")
-    if try_again.lower() == "n":
-        break
+        firstName = random.choice(firstNames)
+        lastName = random.choice(lastNames)
+        print(f"{firstName}{lastName}")
 
-input("Press Enter to exit.")
+        try_again = input("\nTry again? (Press Enter, else press n to quit)\n ")
+        if try_again.lower() == "n":
+            break
+
+    input("Press Enter to exit.")
+
+
